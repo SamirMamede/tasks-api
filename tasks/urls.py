efc929e0911c3c6aeb2cfae6abaa966579b02d8e
SamirMamede <anuarsamir@gmail.com>
2024-04-09 19:48:36 -0400

    fix urlpatterns

--- tasks/urls.py
+++ tasks/urls.py
@@ -31,5 +31,4 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
