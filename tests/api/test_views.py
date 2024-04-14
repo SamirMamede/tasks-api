152dc11195579b50440ecaf6e3ab4589763beeb7
SamirMamede <anuarsamir@gmail.com>
2024-04-13 20:18:44 -0400

    refactoring api tests

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -9,32 +9,32 @@ class TasksViewsTestCase(TestCase):
         self.task1 = Tasks.objects.create(task="Task 1")
         self.task2 = Tasks.objects.create(task="Task 2")
 
-    def test_getTasks(self):
+    def test_get_all_tasks(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
           
-    def test_getTask(self):
-        response = self.client.get(f'tasks/1/')
+    def test_get_task_by_id(self):
+        response = self.client.get('/tasks/1')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         response = self.client.get('tasks/999/')  
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_addTasks(self):
+    def test_create_task(self):
         data = {'task': 'New Task'}
-        response = self.client.post('/tasks/add/', data)
+        response = self.client.post('/tasks/add', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_updateTask(self):
+    def test_update_task(self):
         data = {'task': 'Task Update'}
-        response = self.client.put(f'tasks/{self.task1.pk}/update', data)
+        response = self.client.put('/tasks/1/update', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         response = self.client.put('/tasks/999/update', data)  
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_deleteTask(self):
+    def test_delete_task(self):
         response = self.client.delete(f'/tasks/{self.task1.pk}/delete')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
