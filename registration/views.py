38cf54e6d4383b93e4e1d76661050a65bc492136
SamirMamede <anuarsamir@gmail.com>
2024-05-07 00:33:20 -0400

    usercreate views documentation in swagger

--- registration/views.py
+++ registration/views.py
@@ -2,8 +2,26 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+    @swagger_auto_schema(
+        operation_description="Create a new user",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['username', 'email', 'password'],
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='Username'),
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Email'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Password'),
+            },
+        ),
+        responses={201: 'User created successfully'}
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
